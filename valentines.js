de71636a1ff14726274ee036675b985d89e030a1
Christoffer Jonsson <schnavzer@protonmail.com>
2024-02-01 16:08:54 +0100

    changed url in js

--- valentines.js
+++ valentines.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     var otherButton = document.querySelector(".content button:not(#bouncing)");
 
     otherButton.addEventListener("click", function() {
-        window.location.href = "valentines.html";
+        window.location.href = "https://www.filmstaden.se/"; // Replace "https://example.com" with your desired URL
     });
 });
