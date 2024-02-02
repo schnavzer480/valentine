472bdd1bf180ddcf30334250c23009f6fe1c03dd
Christoffer Jonsson <schnavzer@protonmail.com>
2024-02-02 01:50:52 +0100

    added valentines code to script.js

--- valentines.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var otherButton = document.querySelector(".content button:not(#bouncing)");
-
-    otherButton.addEventListener("click", function() {
-        window.location.href = "https://www.filmstaden.se/"; // Replace "https://example.com" with your desired URL
-    });
-});
